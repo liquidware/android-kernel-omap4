fb725af7f04c487b62581fffa2e543f9d6ef16f9
Andy Green <andy.green@linaro.org>
2011-12-12 17:01:07 +0800

    syntm12xx needs module.h
    Signed-off-by: Andy Green <andy.green@linaro.org>

--- drivers/input/touchscreen/syntm12xx.c
+++ drivers/input/touchscreen/syntm12xx.c
@@ -18,6 +18,7 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <linux/module.h>
 #include <linux/input.h>
 #include <linux/platform_device.h>
 #include <linux/i2c.h>
