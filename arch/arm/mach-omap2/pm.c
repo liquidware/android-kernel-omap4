44af08e99cc87d041c1f7b2ced323a47e69df726
Nishanth Menon <nm@ti.com>
2011-12-12 16:59:23 +0800

    OMAP3+: PM: introduce a central pmic control
    Since we are starting to use multiple PMICs in various combinations,
    use the existing .omap_chip = OMAP_CHIP_INIT() to mark the
    structures we are interested in using per OMAP device we
    are currently running on. This mapping is based on the default
    device recommendations from TI. Boards using custom PMICs now
    have an opportunity to register their own custom mapping.
    
    With this we no longer need omap4_twl_init and omap3_twl_int
    instead we introduce a registration mechanism which is PMIC
    generic and move twl implementation to use the same.
    
    Signed-off-by: Nishanth Menon <nm@ti.com>
    Signed-off-by: Vishwanath BS <vishwanath.bs@ti.com>

--- arch/arm/mach-omap2/pm.c
+++ arch/arm/mach-omap2/pm.c
@@ -227,6 +227,9 @@ postcore_initcall(omap2_common_pm_init);
 
 static int __init omap2_common_pm_late_init(void)
 {
+	/* Init the OMAP TWL parameters */
+	omap_init_all_pmic();
+
 	/* Init the voltage layer */
 	omap_pmic_late_init();
 	omap_voltage_late_init();
