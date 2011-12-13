611b699251d16d688c6ff61b136ae81042cb02d1
Andy Green <andy.green@linaro.org>
2011-12-13 12:29:05 +0800

    omap2 panda add hdmi device
    Signed-off-by: Andy Green <andy.green@linaro.org>

--- arch/arm/mach-omap2/board-omap4panda.c
+++ arch/arm/mach-omap2/board-omap4panda.c
@@ -132,9 +132,15 @@ static void __init panda_leds_init(void)
 	platform_device_register(&leds_gpio);
 }
 
+static struct platform_device omap4panda_hdmi_audio_device = {
+	.name	= "hdmi-audio-dai",
+	.id	= -1,
+};
+
 static struct platform_device *panda_devices[] __initdata = {
 	&wl1271_device,
 	&btwilink_device,
+	&omap4panda_hdmi_audio_device,
 };
 
 static const struct usbhs_omap_board_data usbhs_bdata __initconst = {
