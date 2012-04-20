5884ffdc1a89ed14bd03b48988fccc7f7ad6b4a9
repo sync <chris.ladden@liquidware.com>
2012-04-20 16:22:05 -0400

    Added a compile wrapper script.

---
+++ compile.sh
@@ -0,0 +1,1 @@
+make -j10 ARCH=arm CROSS_COMPILE=/home/eye/linux-omap/omap4/android-toolchain-eabi/bin/arm-linux-androideabi- KCFLAGS="  -fno-pic -mno-unaligned-access" uImage #android_omap4_defconfig uImage
